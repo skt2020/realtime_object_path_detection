ebeb7d553456b7fa34626f4a09a3d3a2e6506d87
skt2020 <44112240+skt2020@users.noreply.github.com>
2019-03-31 17:32:57 +0530

    real time path tracing
    it traces the path of any red object in real time depending upon its hue,velue and seturation ratio it will also affected by environment  hue,velue and seturation ratio

---
+++ OCVTEST/OCVTEST/OCVTEST.cpp
@@ -0,0 +1,127 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//#include "stdafx.h"//header for Visual Studio
+//#include <opencv2/core/core.hpp>//header for OpenCV core
+//#include <opencv2/highgui/highgui.hpp>//header for OpenCV UI
+//#include <iostream>//header for c++ IO
+#include "pch.h"
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	VideoCapture cap(0); //capture the video from webcam
+
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		return -1;
+	}
+
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); 
+
+	int iLowH = 0;
+	int iHighH = 255;
+
+	int iLowS = 152;
+	int iHighS = 253;
+
+	int iLowV = 59;
+	int iHighV = 247;
+
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 255); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 255);
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+	int iLastX = -1;
+	int iLastY = -1;
+
+	//Capture a temporary image from the camera
+	Mat imgTmp;
+	cap.read(imgTmp);
+
+	//Create a black image with the size as the camera output
+	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
+
+
+	while (true)
+	{
+		Mat imgOriginal;
+
+		bool bSuccess = cap.read(imgOriginal); 
+
+
+
+		if (!bSuccess) 
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		Mat imgHSV;
+		 //Convert the captured frame from BGR to HSV
+		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+
+		Mat imgThresholded;
+
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
+
+	
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		
+		Moments oMoments = moments(imgThresholded);
+
+		double dM01 = oMoments.m01;
+		double dM10 = oMoments.m10;
+		double dArea = oMoments.m00;
+
+		
+		if (dArea > 10000)
+		{
+			//calculate the position of the ball
+			int posX = dM10 / dArea;
+			int posY = dM01 / dArea;
+
+			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+			{
+				//Draw a red line from the previous point to the current point
+				line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
+			}
+
+			iLastX = posX;
+			iLastY = posY;
+		}
+                //show the thresholded image
+		imshow("Thresholded Image", imgThresholded); 
+		//show the original imag
+		imgOriginal = imgOriginal + imgLines;
+		imshow("Original", imgOriginal); e
+                //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		if (waitKey(30) == 27) 
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////　
